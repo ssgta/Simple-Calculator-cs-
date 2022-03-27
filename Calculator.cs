1df8de93767dcefb5896ad3eb17e7de8168227d8
ssgta <62765221+ssgta@users.noreply.github.com>
2022-03-27 14:53:27 +0100

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Calculator
+{
+    public static float Addition(float sum, float number) { return sum += number; }
+
+    public static float Subtraction(float sum, float number) { return sum -= number; }
+
+    public static float Multiplication(float sum, float number) { return sum *= number; }
+
+    public static float Division(float sum, float number)
+    {
+        if (number == 0)
+            throw new ArgumentException("Cannot divide by zero.");
+
+        return sum /= number;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using static Calculator;
+
+namespace Calculator_CS
+{
+    class Program
+    {
+        static void Main()
+        {
+            float sum = GetNumber();
+
+            while (true)
+            {
+                string mathOperator = GetMathOperator();
+
+                if (mathOperator is "=")
+                    break;
+
+                float number = GetNumber();
+
+                sum = mathOperator switch
+                {
+                    "+" => Addition(sum, number),
+                    "-" => Subtraction(sum, number),
+                    "/" => Division(sum, number),
+                    "*" => Multiplication(sum, number),
+                    _ => throw new ArgumentException($"Invalid operator: {mathOperator}"
+                )};
+            }
+
+            Console.WriteLine(sum);
+        }
+
+        private static float GetNumber()
+        {
+            Console.WriteLine("Number: ");
+            string input = Console.ReadLine();
+
+            try
+            {
+                float n = float.Parse(input);
+                return n;
+            }
+
+            catch
+            {
+                Console.WriteLine($"Invalid input: '{input}' is not a number.");
+                throw;
+            }
+        }
+
+        private static string GetMathOperator()
+        {
+            Console.WriteLine("Operator (+ - / * =): ");
+            string mathOperator = Console.ReadLine();
+            return mathOperator;
+        }
+    }
+}
